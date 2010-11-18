8ddbf4ff843ac6a7f82449d6a9a1258312a673dd
my12doom <my12doom@2ece6d47-a862-c445-b01b-7049066ce7d0>
2010-11-18 07:58:07 +0000

    Init SsifSource base
    git-svn-id: svn://127.0.0.1@19 2ece6d47-a862-c445-b01b-7049066ce7d0

---
+++ SsifSource/include/Version.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifndef NO_VERSION_REV_NEEDED
+//  #include "Version_rev.h"
+#endif
+
+#define DO_MAKE_STR(x) #x
+#define MAKE_STR(x) DO_MAKE_STR(x)
+
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 4
+#define VERSION_PATCH 0
+#define VERSION_REV 9999
+
+// The date of the DirectX SDK used for compilation used in the error message
+// in mpc-hc and in the installer when the DirectX runtime is out of date
+#define DIRECTX_SDK_DATE "June 2010"
