1d3d4a217f8512d6f00e8f5557b726f8c272d228
my12doom <my12doom@2ece6d47-a862-c445-b01b-7049066ce7d0>
2011-03-25 08:38:15 +0000

    build batch updated.
    git-svn-id: svn://127.0.0.1@64 2ece6d47-a862-c445-b01b-7049066ce7d0

--- build_dwindow.bat
+++ build_dwindow.bat
@@ -4,22 +4,29 @@ set dev2008="C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\devenv"
 E:
 cd\
 cd E:\private_projects
-%dev2003% mySplitter\mySplitter.sln /build "Release_mt"
-%dev2003% dwindow\dwindow.sln /build "Release_mt"
+%dev2008% mySplitter\mySplitter.sln /build "Release_mt"
+%dev2008% dwindow\dwindow.sln /build "Release_mt"
 %dev2003% dwindow_launcher\dwindow_launcher.sln /build "Release"
 %dev2003% pd10\pd10.sln /build "Release"
-%dev2003% ssifavs\ssifavs.sln /build "Release_mt"
+%dev2008% ssifavs\ssifavs.sln /build "Release_mt"
+%dev2008% coreavs\coreavs.sln /build "Release_mt"
 %dev2008% SsifSource\SsifSource.sln /build "Release Filter"
+%dev2008% 3dvSource\3dvSource.sln /build "Release"
+%dev2008% my12doomSource\my12doomSource.sln /build "Release Filter"
 
 #copy
+copy/y dwindow\dwindow.ini dwindow_NSIS
 copy/y dwindow\release_mt\dwindow.exe dwindow_NSIS
 copy/y mySplitter\release_mt\mySplitter.ax dwindow_NSIS\codec
-copy/y dwindow_launcher\release\*.exe dwindow_NSIS
-copy/y SsifSource\bin\Filters_x86\MpegSplitter.ax dwindow_NSIS\codec\SsifSource.ax
+copy/y SsifSource\bin\Filters_x86\SsifSource.ax dwindow_NSIS\codec\SsifSource.ax
+copy/y 3dvSource\Release\3dvSource.ax dwindow_NSIS\codec\3dvSource.ax
+copy/y my12doomSource\bin\Filters_x86\E3DSource.ax dwindow_NSIS\codec\E3DSource.ax
+copy/y my12doomSource\bin\Filters_x86\my12doomSource.ax dwindow_NSIS\codec\my12doomSource.ax
 
 #x264 tools
 copy/y pd10\release\pd10.dll tools\
 copy/y ssifavs\release_mt\ssifavs.dll tools\
+copy/y coreavs\release_mt\coreavs.dll tools\
 
 pause
 F:\NSIS\makensisw.exe dwindow_NSIS\dwindow.nsi
