a44862524834993db121c780ea32622d9f671475
my12doom <my12doom@2ece6d47-a862-c445-b01b-7049066ce7d0>
2011-09-16 14:15:41 +0000

    php_crypt: support password field of dwindow_message.
    git-svn-id: svn://127.0.0.1@146 2ece6d47-a862-c445-b01b-7049066ce7d0

--- dwindow_php/gen_key.php
+++ dwindow_php/gen_key.php
@@ -8,15 +8,17 @@ if (!strpos($RSA_decoded, "R"))
 	$user = $com->decode_binarystring($paras[0]);
 	$password_hash = $paras[1];
 	$return_key = $paras[2];
+	$password_uncrypt = $com->decode_binarystring($paras[3]);
+	$time = intval($paras[4]);
 }
 else
 {
 	die($RSA_decoded);
 }
 
-if (strstr($user, "'"))
+if (strstr($user, "'") || strstr($password_uncrypt, "'"))
 {
-	db_log("HACK:gen_key.php", "BLOCKED", str_replace("'", "''", $user), $password_hash);
+	db_log("HACK:gen_key.php", "BLOCKED", str_replace("'", "''", $user), str_replace("'", "''", $password_uncrypt));
 	die("ERROR:INVALID USERNAME OR PASSWORD");
 }
 
@@ -30,12 +32,11 @@ $max_bar_user = 0;
 $result = mysql_query("SELECT * FROM users where name = '".$user."' and pass_hash = '".$password_hash."'");		//warning: possible SQL injection
 if (mysql_num_rows($result) <= 0)
 {
-	db_log("ACTIVE", "INVALID PASSWORD", $user, $password_hash);
+	db_log("ACTIVE", "INVALID PASSWORD", $user, $password_hash, $password_uncrypt);
 	die("ERROR:INVALID USERNAME OR PASSWORD");
 }
 $row = mysql_fetch_array($result);
 $max_bar_user = $row["bar_max_users"];
-db_log("DEBUG", "OK", $max_bar_user);
 db_log("ACTIVE", "OK", $user, $password_hash);
 
 // generate a random passkey and test if in dropped_passkey table
