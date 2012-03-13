b1260fa2b3eaecde44597ab9dd889ab4f286bc1b
my12doom <my12doom@2ece6d47-a862-c445-b01b-7049066ce7d0>
2012-03-13 16:23:05 +0000

    Audio Downmixer
    git-svn-id: svn://127.0.0.1@225 2ece6d47-a862-c445-b01b-7049066ce7d0

---
+++ mySplitter/audio_downmix.cpp
@@ -0,0 +1,278 @@
+#include "audio_downmix.h"
+
+CDWindowAudioDownmix::CDWindowAudioDownmix(TCHAR *tszName, LPUNKNOWN punk, HRESULT *phr) :
+CTransformFilter(tszName, punk, CLSID_DWindowAudioDownmix)
+{
+} 
+
+CDWindowAudioDownmix::~CDWindowAudioDownmix() 
+{
+}
+
+// CreateInstance
+// Provide the way for COM to create a DWindowExtenderMono object
+CUnknown *CDWindowAudioDownmix::CreateInstance(LPUNKNOWN punk, HRESULT *phr)
+{
+	CDWindowAudioDownmix *pNewObject = new CDWindowAudioDownmix(NAME("DWindow Extender Mono"), punk, phr);
+	// Waste to check for NULL, If new fails, the app is screwed anyway
+	return pNewObject;
+}
+
+STDMETHODIMP CDWindowAudioDownmix::NonDelegatingQueryInterface(REFIID riid, void **ppv)
+{
+	//if (riid == __uuidof(IDWindowExtender)) 
+	//	return GetInterface((IDWindowExtender *) this, ppv);
+
+	return CTransformFilter::NonDelegatingQueryInterface(riid, ppv);
+}
+
+HRESULT CDWindowAudioDownmix::CheckInputType(const CMediaType *mtIn)
+{
+
+	if (mtIn->majortype != MEDIATYPE_Audio)
+		return VFW_E_INVALID_MEDIA_TYPE;
+	if (mtIn->subtype != MEDIASUBTYPE_PCM && mtIn->subtype != MEDIASUBTYPE_IEEE_FLOAT)
+		return VFW_E_INVALID_MEDIA_TYPE;
+	if (mtIn->formattype != FORMAT_WaveFormatEx)
+		return VFW_E_INVALID_MEDIA_TYPE;
+
+	memcpy(&m_in_fmt, mtIn->pbFormat, min(mtIn->cbFormat, sizeof(WAVEFORMATEX)));
+	m_in_subtype = mtIn->subtype;
+
+	m_out_fmt.wFormatTag = WAVE_FORMAT_PCM;
+	m_out_fmt.nChannels = 2;
+	m_out_fmt.nSamplesPerSec = m_in_fmt.nSamplesPerSec;
+	m_out_fmt.nAvgBytesPerSec = 4*m_out_fmt.nSamplesPerSec;
+	m_out_fmt.nBlockAlign = 4;
+	m_out_fmt.wBitsPerSample = 16;
+	m_out_fmt.cbSize = 0;
+	//vihOut->pExtraBytes;
+
+	return NOERROR;
+}
+
+HRESULT CDWindowAudioDownmix::CheckTransform(const CMediaType *mtIn, const CMediaType *mtOut)
+{
+	GUID subtypeIn = *mtIn->Subtype();
+	GUID subtypeOut = *mtOut->Subtype();
+
+
+	return NOERROR;
+}
+
+HRESULT CDWindowAudioDownmix::GetMediaType(int iPosition, CMediaType *pMediaType)
+{
+	// Is the input pin connected
+	if (!m_pInput->IsConnected()) 
+		return E_UNEXPECTED;
+
+	if (iPosition < 0)
+		return E_INVALIDARG;
+
+	// Do we have more items to offer
+	if (iPosition > 0)
+		return VFW_S_NO_MORE_ITEMS;
+
+	CMediaType *inMediaType = &m_pInput->CurrentMediaType();
+	pMediaType->SetType(&MEDIATYPE_Audio);
+	pMediaType->SetSubtype(&MEDIASUBTYPE_PCM);
+	pMediaType->SetFormatType(&FORMAT_WaveFormatEx);
+	pMediaType->bFixedSizeSamples = TRUE;
+	pMediaType->SetTemporalCompression(FALSE);
+	pMediaType->SetSampleSize(16);
+
+	WAVEFORMATEX *vihOut = (WAVEFORMATEX *)pMediaType->ReallocFormatBuffer(sizeof(WAVEFORMATEX));
+	memcpy(vihOut, &m_out_fmt, sizeof(WAVEFORMATEX));
+
+
+	return NOERROR;
+}
+
+HRESULT CDWindowAudioDownmix::DecideBufferSize(IMemAllocator *pAlloc,ALLOCATOR_PROPERTIES *pProperties)
+{
+	// Is the input pin connected
+	if (!m_pInput->IsConnected()) 
+		return E_UNEXPECTED;
+
+	HRESULT hr = NOERROR;
+
+	CMediaType *inMediaType = &m_pInput->CurrentMediaType();
+
+	pProperties->cBuffers = 4;
+	pProperties->cbBuffer = 307200;
+
+	ALLOCATOR_PROPERTIES Actual;
+	hr = pAlloc->SetProperties(pProperties,&Actual);
+	if (FAILED(hr)) return hr;
+	if (pProperties->cBuffers > Actual.cBuffers || pProperties->cbBuffer > Actual.cbBuffer) 
+		return E_FAIL;
+
+
+	return NOERROR;
+}
+
+inline void CDWindowAudioDownmix::mix_a_sample_PCM(void* in, void *out)
+{
+	short* src = (short*)in;
+	short L = src[0];
+	short R = src[1];
+	short C = src[2];
+	short LFE = src[3];
+	short BL = src[4];
+	short BR = src[5];
+
+	short *dst = (short*)out;
+	dst[0] = (short) ((L*0.2929 + C*0.2071 + BL*0.2929 + LFE*0.2071));
+	dst[1] = (short) ((R*0.2929 + C*0.2071 + BR*0.2929 + LFE*0.2071));
+}
+
+inline void CDWindowAudioDownmix::mix_a_sample_PCM2(void* in, void *out)
+{
+	short * src = (short*)in;
+	short *dst = (short*)out;
+
+	dst[0] = src[0];
+	dst[1] = src[1];
+}
+
+inline void CDWindowAudioDownmix::mix_a_sample_PCM_24bit(void* in, void *out)
+{
+	BYTE test[18];
+	memcpy(test, in, 18);
+
+	short L = (*(short*)((BYTE*)in+1+0*3));
+	short R = (*(short*)((BYTE*)in+1+1*3));
+	short C = (*(short*)((BYTE*)in+1+2*3));
+	short LFE = (*(short*)((BYTE*)in+1+3*3));
+	short BL = (*(short*)((BYTE*)in+1+4*3));
+	short BR = (*(short*)((BYTE*)in+1+5*3));
+
+	short *dst = (short*)out;
+	dst[0] = (short) ((L*0.2929 + C*0.2071 + BL*0.2929 + LFE*0.2071));
+	dst[1] = (short) ((R*0.2929 + C*0.2071 + BR*0.2929 + LFE*0.2071));
+}
+
+inline void CDWindowAudioDownmix::mix_a_sample_PCM2_24bit(void* in, void *out)
+{
+	short *dst = (short*)out;
+	dst[0] = (*(short*)((BYTE*)in+1+0*3));
+	dst[1] = (*(short*)((BYTE*)in+1+1*3));
+}
+
+inline void CDWindowAudioDownmix::mix_a_sample_PCM2_8bit(void* in, void *out)
+{
+	BYTE * src = (BYTE*)in;
+	short *dst = (short*)out;
+
+	dst[0] = (((int)src[0])-128) * 256;
+	dst[1] = (((int)src[1])-128) * 256;
+}
+
+inline float clip_float(float in)
+{
+	if (in<-1)
+		return -1;
+	if (in>1)
+		return 1;
+	return in;
+}
+
+inline void CDWindowAudioDownmix::mix_a_sample_FLOAT(void* in, void *out)
+{
+	float * src = (float*)in;
+	float L = clip_float(src[0]);
+	float R = clip_float(src[1]);
+	float C = clip_float(src[2]);
+	float LFE = clip_float(src[3]);
+	float BL = clip_float(src[4]);
+	float BR = clip_float(src[5]);
+
+	signed short *dst = (signed short*)out;
+	dst[0] = (signed short) (32767*(L*0.2929 + C*0.2071 + BL*0.2929 + LFE*0.2071));
+	dst[1] = (signed short) (32767*(R*0.2929 + C*0.2071 + BR*0.2929 + LFE*0.2071));
+}
+
+inline void CDWindowAudioDownmix::mix_a_sample_FLOAT2(void* in, void *out)
+{
+	float * src = (float*)in;
+	signed short *dst = (signed short*)out;
+
+	dst[0] = (signed short) (clip_float(src[0]) * 32767);
+	dst[1] = (signed short) (clip_float(src[1]) * 32767);
+}
+
+
+#define do_convert(src, dst, func) \
+	for(int i=0; i<sample_count; i++)\
+	{\
+		func(src, dst);\
+		src += m_in_fmt.nBlockAlign;\
+		dst += m_out_fmt.nBlockAlign;\
+	}\
+
+HRESULT CDWindowAudioDownmix::Transform(IMediaSample *pIn, IMediaSample *pOut)
+{
+	BYTE *src;
+	BYTE *dst;
+
+	pIn->GetPointer(&src);
+	pOut->GetPointer(&dst);
+
+	int sample_count = pIn->GetActualDataLength() / m_in_fmt.nBlockAlign;
+	pOut->SetActualDataLength(sample_count * m_out_fmt.nBlockAlign);
+
+	if (m_in_subtype == MEDIASUBTYPE_IEEE_FLOAT)
+	{
+		if (m_in_fmt.nChannels >= 6)
+		{
+			do_convert(src, dst, mix_a_sample_FLOAT);
+		}
+		else
+		{
+			do_convert(src, dst, mix_a_sample_FLOAT2);
+		}
+	}
+	else
+	{
+		if (m_in_fmt.nChannels >= 6)
+		{
+			if (m_in_fmt.wBitsPerSample == 16)
+			{
+				do_convert(src, dst, mix_a_sample_PCM);
+			}
+			else if (m_in_fmt.wBitsPerSample == 24)
+			{
+				do_convert(src, dst, mix_a_sample_PCM_24bit);
+			}
+		}
+		else
+		{
+			if (m_in_fmt.wBitsPerSample == 16)
+			{
+				do_convert(src, dst, mix_a_sample_PCM2);
+			}
+			else if (m_in_fmt.wBitsPerSample == 24)
+			{
+				do_convert(src, dst, mix_a_sample_PCM2_24bit);
+			}
+			else if (m_in_fmt.wBitsPerSample == 8)
+			{
+				do_convert(src, dst, mix_a_sample_PCM2_8bit);
+			}
+		}
+	}
+		
+
+	REFERENCE_TIME TimeStart, TimeEnd;
+	pIn->GetTime(&TimeStart, &TimeEnd);
+	LONGLONG MediaStart, MediaEnd;
+	pIn->GetMediaTime(&MediaStart,&MediaEnd);
+	pOut->SetTime(&TimeStart, &TimeEnd);
+	pOut->SetMediaTime(&MediaStart,&MediaEnd);
+	pOut->SetSyncPoint(pIn->IsSyncPoint() == S_OK);
+	pOut->SetPreroll(pIn->IsPreroll() == S_OK);
+	pOut->SetDiscontinuity(pIn->IsDiscontinuity() == S_OK);
+
+
+	return S_OK;
+}
---
+++ mySplitter/audio_downmix.h
@@ -0,0 +1,39 @@
+#include <streams.h>
+
+// {61FEB42B-6680-411b-8378-A93483889056}
+DEFINE_GUID(CLSID_DWindowAudioDownmix, 
+			0x61feb42b, 0x6680, 0x411b, 0x83, 0x78, 0xa9, 0x34, 0x83, 0x88, 0x90, 0x56);
+
+class CDWindowAudioDownmix : CTransformFilter
+{
+public:
+	DECLARE_IUNKNOWN;
+	static CUnknown * WINAPI CreateInstance(LPUNKNOWN punk, HRESULT *phr);
+
+	// Reveals IDWindowExtenderMono and ISpecifyPropertyPages
+	STDMETHODIMP NonDelegatingQueryInterface(REFIID riid, void ** ppv);
+
+	// Overrriden from CSplitFilter base class
+	HRESULT Transform(IMediaSample * pIn, IMediaSample *pOut);
+	HRESULT CheckInputType(const CMediaType *mtIn);
+	HRESULT CheckTransform(const CMediaType *mtIn, const CMediaType *mtOut);
+	HRESULT DecideBufferSize(IMemAllocator *pAlloc, ALLOCATOR_PROPERTIES *pProperties);
+	HRESULT GetMediaType(int iPosition, CMediaType *pMediaType);
+
+	// you know what
+	CDWindowAudioDownmix(TCHAR *tszName, LPUNKNOWN punk, HRESULT *phr);
+	~CDWindowAudioDownmix();
+
+protected:
+	GUID m_in_subtype;
+	WAVEFORMATEX m_out_fmt;
+	WAVEFORMATEX m_in_fmt;
+
+	inline void mix_a_sample_PCM(void* in, void *out);
+	inline void mix_a_sample_PCM2(void* in, void *out);				// just copy
+	inline void mix_a_sample_PCM_24bit(void* in, void *out);
+	inline void mix_a_sample_PCM2_24bit(void* in, void *out);		// just copy up 16bit
+	inline void mix_a_sample_PCM2_8bit(void* in, void *out);		// just copy up 16bit
+	inline void mix_a_sample_FLOAT(void* in, void *out);
+	inline void mix_a_sample_FLOAT2(void* in, void *out);	// just convert first 2 channel
+};
