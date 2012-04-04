18f2341ca4b0de520744ca3e2cb93484a5a6e07f
my12doom <my12doom@2ece6d47-a862-c445-b01b-7049066ce7d0>
2012-04-04 06:55:28 +0000

    backup update
    git-svn-id: svn://127.0.0.1@244 2ece6d47-a862-c445-b01b-7049066ce7d0

--- dwindow_php/backup.bat
+++ dwindow_php/backup.bat
@@ -1,7 +1,7 @@
 set v=%DATE:~2,2%%DATE:~5,2%%DATE:~8,2%_%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%.sql
 set v2=%DATE:~2,2%%DATE:~5,2%%DATE:~8,2%_%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%.rar
-"C:\Program Files\MySQL\MySQL Server 5.5\bin\mysqldump.exe" -u root -ptester88 mydb >> %v%
-rar a -ptester88 %v2% %v%
-del %v%
+"D:\Program Files\MySQL\MySQL Server 5.5\bin\mysqldump.exe" -u root -ptester88 mydb >>1.sql
+rar a -ptester88 %v2% 1.sql
+del 1.sql
 copy %v2% G:\sqlbackup
 move %v2% backup
