de544ee1596af1a87da74f49558b618eb22aada3
my12doom <my12doom@2ece6d47-a862-c445-b01b-7049066ce7d0>
2013-02-25 10:00:49 +0000

    progress
    git-svn-id: svn://127.0.0.1@586 2ece6d47-a862-c445-b01b-7049066ce7d0

--- Thumbnail/client.cpp
+++ Thumbnail/client.cpp
@@ -8,6 +8,26 @@ HRESULT save_bitmap(void *data, const wchar_t *filename, int width, int height);
 
 int main()
 {
+	wchar_t worker_path[MAX_PATH];
+	GetModuleFileNameW(NULL, worker_path, MAX_PATH);
+	wcscpy(wcsrchr(worker_path, L'\\'), L"\\Thumbnail.exe");
+	SHELLEXECUTEINFO ShExecInfo = {0};
+	ShExecInfo.cbSize = sizeof(SHELLEXECUTEINFO);
+	ShExecInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
+	ShExecInfo.hwnd = NULL;
+	ShExecInfo.lpFile = worker_path;
+	ShExecInfo.lpParameters = NULL;	
+	ShExecInfo.lpDirectory = NULL;
+	ShExecInfo.nShow = SW_SHOW;
+	ShExecInfo.hInstApp = NULL;	
+	ShExecInfo.lpVerb = L"open";
+
+	ShellExecuteEx(&ShExecInfo);
+	HANDLE process = ShExecInfo.hProcess;
+
+	Sleep(500);
+
+
 	remote_thumbnail r;
 	r.set_out_format(30, 400, 225);
 	r.connect(L"\\\\.\\pipe\\DWindowThumbnailPipe");
@@ -23,6 +43,8 @@ int main()
 	}
 	r.close();
 	r.disconnect();
+
+	TerminateProcess(process, 0);
 
 	return 0;
 }
