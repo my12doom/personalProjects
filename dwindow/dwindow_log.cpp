67f1a04664d168dab7a021f81ce75d9a0cb1e727
my12doom <my12doom@2ece6d47-a862-c445-b01b-7049066ce7d0>
2013-03-27 13:49:17 +0000

    git-svn-id: svn://127.0.0.1@660 2ece6d47-a862-c445-b01b-7049066ce7d0

--- dwindow/dwindow_log.cpp
+++ dwindow/dwindow_log.cpp
@@ -55,7 +55,7 @@ int dwindow_log_line(const char *format, ...)
 	time_t tt = time_t(NULL);
 	struct tm t = *localtime(&tt);
 	wchar_t time_str[200];	
-	wsprintfW(time_str, L"%d-%02d-%02d %02d:%02d:%02d:%03d", t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec, GetTickCount()%1000);
+	wsprintfW(time_str, L"%d-%02d-%02d %02d:%02d:%02d:%03d ", t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec, GetTickCount()%1000);
 	fwrite(time_str, 2, wcslen(time_str), f);
 
 	USES_CONVERSION;
