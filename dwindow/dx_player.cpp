562cbb1a86b9458c9a46f694f4addfde399e55bb
my12doom <my12doom@2ece6d47-a862-c445-b01b-7049066ce7d0>
2013-10-30 06:19:16 +0000

    minor change.
    git-svn-id: svn://127.0.0.1@930 2ece6d47-a862-c445-b01b-7049066ce7d0

--- dwindow/dx_player.cpp
+++ dwindow/dx_player.cpp
@@ -2924,7 +2924,8 @@ HRESULT dx_player::exit_direct_show()
 	m_file_loaded = false;
 	m_active_audio_track = 0;
 	m_active_subtitle_track = 0;
-	m_total_time = m_current_time = 0;
+	m_current_time = 0;
+	m_total_time = 1;
 	m_filter_state = -1;
 	
 	m_offset_metadata = NULL;
